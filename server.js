e1ad1833853e011cacc5f3e4fd6a5f40f8d2514a
Kelrynn <cameroncastells@gmail.com>
2017-10-08 14:54:56 -0600

    adding seach functionality

--- server.js
+++ server.js
@@ -16,11 +16,11 @@ app.use(express.static(__dirname + '/public'));
 
 // our database is an array for now with some hardcoded values
 var todos = [
-  // { _id: 1, task: 'Laundry', description: 'Wash clothes' },
-  // { _id: 2, task: 'Grocery Shopping', description: 'Buy dinner for this week' },
-  // { _id: 3, task: 'Homework', description: 'Make this app super awesome!' }
+  { _id: 1, task: 'Laundry', description: 'Wash clothes' },
+  { _id: 2, task: 'Grocery Shopping', description: 'Buy dinner for this week' },
+  { _id: 3, task: 'Homework', description: 'Make this app super awesome!' }
 ];
-
+var idCounter = todos.length +1;
 /**********
  * ROUTES *
  **********/
@@ -48,23 +48,40 @@ app.get('/api/todos/search', function search(req, res) {
   /* This endpoint responds with the search results from the
    * query in the request. COMPLETE THIS ENDPOINT LAST.
    */
+   var toDos = todos.filter((todo) => {
+    return req.query[Object.keys((req.query))[0]] === todo.task || req.query[Object.keys((req.query))[0]] === todo.description;
+   });
+   res.json({todos: toDos});
 });
 
 app.get('/api/todos', function index(req, res) {
   /* This endpoint responds with all of the todos
    */
+   res.json({todos});
 });
 
 app.post('/api/todos', function create(req, res) {
   /* This endpoint will add a todo to our "database"
    * and respond with the newly created todo.
    */
+   console.log(req.body);
+   var todo = {
+      _id: idCounter++,
+      task: req.body.task,
+      description: req.body.description
+   };
+   todos.push(todo);
+   res.json(todo);
 });
 
 app.get('/api/todos/:id', function show(req, res) {
   /* This endpoint will return a single todo with the
    * id specified in the route parameter (:id)
    */
+   var todoIndex = todos.find((obj) => {
+     return obj._id == req.params.id;
+   });
+   res.json(todoIndex);
 });
 
 app.put('/api/todos/:id', function update(req, res) {
@@ -72,6 +89,16 @@ app.put('/api/todos/:id', function update(req, res) {
    * id specified in the route parameter (:id) and respond
    * with the newly updated todo.
    */
+  var todo = todos.find((obj) => {
+    return obj._id == req.params.id;
+   });
+  var index = todos.indexOf(todo);
+  todo = req.body;
+  todo._id = Number(req.params.id);
+  todos[index] = todo;
+  res.json(todo);
+
+
 });
 
 app.delete('/api/todos/:id', function destroy(req, res) {
@@ -79,6 +106,13 @@ app.delete('/api/todos/:id', function destroy(req, res) {
    * id specified in the route parameter (:id) and respond
    * with deleted todo.
    */
+   var todo = todos.find((obj) => {
+    return obj._id == req.params.id;
+   });
+   var index = todos.indexOf(todo);
+   todos.splice(index, 1);
+   res.json(todo);
+
 });
 
 /**********
